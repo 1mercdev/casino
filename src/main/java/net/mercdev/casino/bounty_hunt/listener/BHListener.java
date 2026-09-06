421a0fd91500d9a7f07aa07422af7350f6e693b0
1mercdev <1mercdev@gmail.com>
2026-09-07 01:03:54 +0200

    feat: added listener and manager classes

--- src/main/java/net/mercdev/casino/bounty_hunt/listener/BHListener.java
+++ src/main/java/net/mercdev/casino/bounty_hunt/listener/BHListener.java
@@ -1,5 +1,39 @@
 package net.mercdev.casino.bounty_hunt.listener;
 
-public class BHListener {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.entity.Player;
+
+import net.mercdev.casino.bounty_hunt.BountyHunt;
+
+public class BHListener implements Listener{
     
+    private final BountyHunt bHunt;
+
+    public BHListener(BountyHunt bHunt) {
+        this.bHunt = bHunt;
+    }
+
+    @EventHandler 
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player victim = event.getEntity();
+        Player killer = victim.getKiller();
+
+        if (killer != null) {
+            bHunt.getBountyManager().tryBountyClaim(killer, victim);
+        }
+    }
+
+    @EventHandler 
+    public void onJoin(PlayerJoinEvent event) {
+        bHunt.getBountyManager().loadPlayer(event.getPlayer());
+    }
+
+    @EventHandler 
+    public void onQuit(PlayerQuitEvent event) {
+        bHunt.getBountyManager().unloadPlayer(event.getPlayer());
+    }
 }
